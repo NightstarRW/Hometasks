f3de515be98a0080469718ad0f17672ec2ed8284
Maksim Serada <heavynightsnmail@gmail.com>
2020-12-27 12:55:05 +0300

    add java clean code task

---
+++ JavaCleanCode/src/main/java/Planes/ExperimentalPlane.java
@@ -0,0 +1,43 @@
+package Planes;
+
+import models.ClassificationLevel;
+import models.ExperimentalTypes;
+
+public class ExperimentalPlane extends Plane {
+    private final ExperimentalTypes type;
+    private ClassificationLevel classificationLevel;
+    private final String model;
+
+    public ExperimentalPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity,
+                             ExperimentalTypes type, ClassificationLevel classificationLevel) {
+        super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
+        this.type = type;
+        this.classificationLevel = classificationLevel;
+        this.model = model;
+    }
+
+    public ClassificationLevel getClassificationLevel() {
+        return classificationLevel;
+    }
+
+    public void setClassificationLevel(ClassificationLevel classificationLevel) {
+        this.classificationLevel = classificationLevel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "experimentalPlane{" +
+                "model='" + model + '\'' +
+                '}';
+    }
+}
