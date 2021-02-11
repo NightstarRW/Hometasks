ba5632193eb113193daa8e8e828291282b635ab0
Maksim Serada <heavynightsnmail@gmail.com>
2021-02-11 16:06:53 +0300

    name fix

--- Framework/src/test/java/service/TestDataReader.java
+++ Framework/src/test/java/service/TestDataReader.java
@@ -3,7 +3,7 @@ package service;
 import java.util.ResourceBundle;
 
 public class TestDataReader {
-    private static final ResourceBundle resourceBundle = ResourceBundle.getBundle(System.getProperty("computeEngine"));
+    private static final ResourceBundle resourceBundle = ResourceBundle.getBundle(System.getProperty("engine"));
 
     public static String getTestData(String key) {
         return resourceBundle.getString(key);
